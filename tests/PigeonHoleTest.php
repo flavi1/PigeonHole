bd1752b40cfa28dbe114b9bc6ea379d69ea3526a
flavi1 <flavien.guillon@gmail.com>
2023-09-01 14:31:06 +0200

    Update PigeonHoleTest.php

--- tests/PigeonHoleTest.php
+++ tests/PigeonHoleTest.php
@@ -1,5 +1,12 @@
 <?php
+namespace PigeonHole;
 
-class PigeonHoleTest extends {
-
+class PigeonHoleTest extends \PHPUnit\Framework\TestCase {
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
 }
